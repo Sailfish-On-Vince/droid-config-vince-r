4656c572286578764f4ae430f5002812069b9bf8
BirdZhang <0312birdzhang@gmail.com>
2018-06-05 21:46:59 +0800

    Update droid-hal-startup.sh

--- sparse/usr/bin/droid/droid-hal-startup.sh
+++ sparse/usr/bin/droid/droid-hal-startup.sh
@@ -6,8 +6,5 @@ export LD_LIBRARY_PATH=/usr/libexec/droid-hybris/system/lib/:/vendor/lib:/system
 # Save systemd notify socket name to let droid-init-done.sh pick it up later
 echo $NOTIFY_SOCKET > /run/droid-hal/notify-socket-name
 
-+# Use exec nohup since systemd may send SIGHUP, but droid-hal-init doesn't
-+# handle it. This avoids having to modify android_system_core, which would
-+# require different handling for every different android version.
-+exec nohup /sbin/droid-hal-init
+/sbin/droid-hal-init
 
