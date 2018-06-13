1f909308028d4f4d463e0edbcddceb849a19760f
BirdZhang <0312birdzhang@gmail.com>
2018-06-13 11:52:11 +0800

    if /data/media/0 not exist then use /data/media

--- sparse/usr/bin/droid/android-links.sh
+++ sparse/usr/bin/droid/android-links.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 if [ ! -L /home/nemo/android_storage ]; then
-    ln -s /data/media/0 /home/nemo/android_storage
+    if [ ! -d /data/media/0 ]; then
+        ln -s /data/media /home/nemo/android_storage
+    else
+        ln -s /data/media/0 /home/nemo/android_storage
+    fi
 fi
  
