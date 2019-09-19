142c83bbeac7486dfe9151790332a81db875f8b3
BirdZhang <0312birdzhang@gmail.com>
2019-09-19 10:51:30 +0000

    Add get bt address
    Change-Id: I8175043909148be3a8f2559ec42a59d65e3c07f6

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 bt_mac=$(/vendor/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oE '([0-9a-f]{2}:){5}([0-9a-f]{2})')
-echo "BT MAC: $bt_mac"
 if [ ! -z "$bt_mac" ] ; then
      echo $bt_mac > /var/lib/bluetooth/board-address
      echo "BT MAC: $bt_mac"
+else
+  if [ -f /data/misc/bluedroid/bt_config.conf ];then
+    bt_mac=$(cat /data/misc/bluedroid/bt_config.conf | grep Address|awk -F' = ' '{print $2}')
+    echo $bt_mac > /var/lib/bluetooth/board-address
+  fi
 fi
