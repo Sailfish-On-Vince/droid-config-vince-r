a8c358ea41149472591fa68d7b7f2654a077c4d1
BirdZhang <0312birdzhang@gmail.com>
2019-10-31 08:58:52 +0000

    fix ld.config mount

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-if ! grep -q hybris /system/etc/ld.config.28.txt; then
-    mount -o bind /usr/libexec/droid-hybris/system/etc/ld.config.28.txt /system/etc/ld.config.28.txt
-fi
+echo "pass"
